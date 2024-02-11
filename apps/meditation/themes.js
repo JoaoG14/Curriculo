683f38b48f47321e8732e9ea8080c46af4fc6c2b
João <joaog.alves14@gmail.com>
2024-02-11 18:56:37 -0300

    Added new settings

--- apps/meditation/themes.js
+++ apps/meditation/themes.js
@@ -6,22 +6,22 @@ function checkTheme() {
 window.onload = checkTheme;
 
 const selected = document.querySelector(".selected");
-const optionsContainer = document.querySelector(".options-container");
+const themesContainer = document.querySelector(".options-container");
 const html = document.documentElement;
 const settin = document.querySelector("#focus");
 
 let themeSelected = localStorage.getItem("themeSelected");
 
-const optionsList = document.querySelectorAll(".option");
+const themesList = document.querySelectorAll(".option");
 
 selected.addEventListener("click", () => {
-  optionsContainer.classList.toggle("active");
+  themesContainer.classList.toggle("active");
 });
 
-optionsList.forEach((o) => {
+themesList.forEach((o) => {
   o.addEventListener("click", () => {
     selected.innerHTML = o.querySelector("label").innerHTML;
-    optionsContainer.classList.remove("active");
+    themesContainer.classList.remove("active");
     themeSelected = o.querySelector("label").innerHTML;
     html.id = o.querySelector("label").innerHTML;
     console.log(themeSelected);
