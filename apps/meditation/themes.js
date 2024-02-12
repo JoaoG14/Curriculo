e19bf187ae8dd2b2d83768ed8fcb09c718a4f24e
João <joaog.alves14@gmail.com>
2024-02-12 13:46:31 -0300

    Reversed Settings Chnages

--- apps/meditation/themes.js
+++ apps/meditation/themes.js
@@ -1,11 +1,10 @@
-function checkTheme() {
+function checkSettings() {
   html.id = themeSelected;
   selected.innerHTML = themeSelected;
+  selectedStyle.innerHTML = styleSelected;
 }
 
-window.onload = checkTheme;
-
-
+window.onload = checkSettings;
 
 const selected = document.querySelector(".selected-theme");
 const themesContainer = document.querySelector(".options-container-themes");
@@ -27,5 +26,26 @@ themesList.forEach((o) => {
     html.id = o.querySelector("label").innerHTML;
     console.log(themeSelected);
     localStorage.setItem("themeSelected", o.querySelector("label").innerHTML);
+  });
+});
+
+const selectedStyle = document.querySelector(".selected-style");
+const stylesContainer = document.querySelector(".options-container-styles");
+
+let styleSelected = localStorage.getItem("styleSelected");
+
+const stylesList = document.querySelectorAll(".style-option");
+
+selectedStyle.addEventListener("click", () => {
+  stylesContainer.classList.toggle("active");
+});
+
+stylesList.forEach((o) => {
+  o.addEventListener("click", () => {
+    selectedStyle.innerHTML = o.querySelector("label").innerHTML;
+    stylesContainer.classList.remove("active");
+    styleSelected = o.querySelector("label").innerHTML;
+    localStorage.setItem("styleSelected", o.querySelector("label").innerHTML);
+    console.log(styleSelected);
   });
 });
